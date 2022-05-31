8ecd5ddc1e5af9aefd037944aba5bb1c74658e28
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-31 23:31:27 +0300

    Trying to append all content of restaurant view

--- JS/ViewUsers.js
+++ JS/ViewUsers.js
@@ -19,11 +19,14 @@ function createAppendUserRows(data) {
     let user_name_td = document.createElement("td");
     user_name_td.innerText = `${element.first_name} ${element.last_name}`;
     user_row.append(user_name_td);
+
     let user_email_td = document.createElement("td");
     user_email_td.innerText = element.email;
     user_row.append(user_email_td);
+    
     let user_dob_td = document.createElement("td");
     user_dob_td.innerText = element.dob;
+
     user_row.append(user_dob_td);
     table.append(user_row);
   });
