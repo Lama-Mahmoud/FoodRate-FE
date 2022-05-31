1dfd61daf4d471c4555f0e9691ad64e3128dee8b
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-31 08:44:56 +0300

    Adding JS files pending comments

---
+++ JS/pendingComments.js
@@ -0,0 +1,33 @@
+window.onload=function(){
+
+	
+	function openNav() {
+	  document.getElementById("mySidenav").style.display = "block";
+	}
+
+	function closeNav() {
+	  document.getElementById("mySidenav").style.display = "none";
+	}
+	
+	accept=document.getElementsByClassName("true");
+	
+	reject=document.getElementsByClassName("false");
+	
+	for(let i=0;i<reject.length;i++)
+	{
+		console.log("added");
+		accept[i].addEventListener("click",accepted);
+		reject[i].addEventListener("click",rejected);
+	}
+		
+		
+		
+		function accepted()
+		{
+			alert("comment accepted");
+		}
+		function rejected()
+		{
+			alert("comment rejected");
+		}
+}
