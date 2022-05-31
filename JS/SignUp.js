5d2710fcfc2d371bf702c2ed318b75c9fabfcade
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-31 20:21:09 +0300

    adding axios

--- JS/SignUp.js
+++ JS/SignUp.js
@@ -1,21 +1,29 @@
+
 const email_field = document.getElementById("email"),
   password_field = document.getElementById("psw"),
   first_name=document.getElementById("fname"),
   last_name=document.getElementById("lname"),
   DOB=document.getElementById("lname"),
-  gender=document.querySelector('input[name="rate"]:checked').value;
   profile_pic=document.getElementById('getFile').files[0];
   submit_btn = document.getElementById("signup");
-
+  
   
 submit_btn.onclick = () => {
-	
+	var gender;
+    function displayRadioValue() {
+        let ele = document.getElementsByName('gender');
+          
+        for(i = 0; i < ele.length; i++) {
+            if(ele[i].checked)
+            gender=ele[i].value;
+        }
+    }
     let data = new FormData();
     data.append("email", email_field.value);
     data.append("password", password_field.value);
-    data.append("first_name",first_name);
-    data.append("last_name",last_name);
-    data.append("dob",DOB);
+    data.append("first_name",first_name.value);
+    data.append("last_name",last_name.value);
+    data.append("dob",DOB.value);
     data.append("gender",gender);
     data.append("profile_pic",profile_pic);
     console.log(data);
@@ -24,6 +32,7 @@ submit_btn.onclick = () => {
         method: "post",
         url: "http://127.0.0.1/api/index.php?action=createUser",
         data: data,
+      }).then(function (affected_rows) {
+          console.log(affected_rows);
       });
-
 };
