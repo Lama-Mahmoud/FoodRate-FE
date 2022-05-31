edce6cdcc409717469091c0dd11f122a22fe98a6
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-31 23:41:07 +0300

    Update profile user

--- JS/EditUser.js
+++ JS/EditUser.js
@@ -30,9 +30,27 @@ submit_btn.onclick = () => {
 first_name=document.getElementById("fname").value,
 last_name=document.getElementById("lname").value,
 profile_pic=document.getElementById("getFile").files[0];
+if(!profile_pic)
+{
+    profile_pic=users_data.profile_pic;
+}
+
+let datanew = new FormData();
+datanew.append("email", email_field.value);
+datanew.append("password", password_field.value);
+atanew.append("first_name",first_name.value);
+atanew.append("last_name",last_name.value);
+atanew.append("dob",data.dob);
+atanew.append("gender",data.gender);
+atanew.append("profile_pic",profile_pic);
 
 axios({
-    
 
-});
+    method: "post",
+    url: "http://127.0.0.1/api/index.php?action=updateUser&user_id=${user_id}",
+    data: data,
+    headers: headers,
+  }).then(function (affected_rows) {
+      console.log(affected_rows);
+  });
 }
